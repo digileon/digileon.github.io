ca6d4fbf37104a0c21bdcaefeb3f0611e8c65686
SubHero <github@bvdsoftware.be>
2018-08-24 13:33:55 +0200

    Fixes

---
+++ niels/script.js
@@ -0,0 +1,42 @@
+image1 = document.querySelector("#img1")
+image1.addEventListener("click", function () {
+    if (image1.style.width == "400px") {
+        image1.style.width = 100;
+        image1.style.height = 75;
+    } else {
+    image1.style.width = 400
+    image1.style.height = 300
+    }
+})
+image2 = document.querySelector("#img2")
+image2.addEventListener("click", function () {
+    if (image2.style.width == "400px") {
+        image2.style.width = 100;
+        image2.style.height = 75;
+    } else {
+    image2.style.width = 400
+    image2.style.height = 300
+    }
+})
+image3 = document.querySelector("#img3")
+image3.addEventListener("click", function () {
+    if (image3.style.width == "400px") {
+        image3.style.width = 100;
+        image3.style.height = 75;
+    } else {
+    image3.style.width = 400
+    image3.style.height = 300
+    }
+})
+image4 = document.querySelector("#img4")
+image4.addEventListener("click", function () {
+    if (image4.style.width == "400px") {
+        image4.style.width = 100;
+        image4.style.height = 75;
+    } else {
+        image4.style.width = 400
+        image4.style.height = 300
+    }
+
+
+})
